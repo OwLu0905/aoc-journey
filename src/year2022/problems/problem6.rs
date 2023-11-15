bde4f56c84d70a2a11f1eb26f6dfd3cd24649e6f
OwLu0905 <owlu0905@gmail.com>
2023-11-15 18:42:06 +0800

    add 2022/p6_1 method2

--- src/year2022/problems/problem6.rs
+++ src/year2022/problems/problem6.rs
@@ -13,25 +13,18 @@ pub fn problem6_1(puzzle_path: &str) -> i32 {
     let mut check_is_duplicate: Vec<bool> = vec![false; puzzle.chars().count()];
 
     // TODO: use vec to minimize memory size
-    let mut check_record: HashMap<char, usize> = HashMap::new();
+    // let mut check_record: HashMap<char, usize> = HashMap::new();
+    let mut check_record: Vec<Option<usize>> = vec![None; 128];
 
     for (i, val) in puzzle.chars().enumerate() {
-        match check_record.get(&val) {
-            Some(&prev_index) => {
-                if (i - prev_index) >= 4 {
-                    check_is_duplicate[prev_index] = false;
-                } else {
-                    check_is_duplicate[prev_index] = true;
-                }
-            }
-            None => {
-                check_record.insert(val, i);
+        if let Some(prev_index) = check_record[val as usize] {
+            if (i - prev_index) >= 4 {
+                check_is_duplicate[prev_index] = false;
+            } else {
+                check_is_duplicate[prev_index] = true;
             }
         }
-
-        if let Some(v) = check_record.get_mut(&val) {
-            *v = i
-        }
+        check_record[val as usize] = Some(i);
     }
 
     // NOTE: count
@@ -53,6 +46,53 @@ pub fn problem6_1(puzzle_path: &str) -> i32 {
     }
     result_index + 1
 }
+
+pub fn problem6_1_another(puzzle_path: &str) -> i32 {
+    let mut file = File::open(puzzle_path).expect("can't read file");
+    let mut puzzle = String::new();
+    let _ = file.read_to_string(&mut puzzle);
+    puzzle = puzzle
+        .trim_end_matches(|c| c == '\n' || c == '\r')
+        .to_string();
+
+    let mut count: i32 = 0;
+    let mut point: i32 = 0;
+
+    let mut count_map: HashMap<char, i32> = HashMap::new();
+
+    let puzzle_len = puzzle.chars().count() as i32;
+    let puzzle_char = puzzle.chars().collect::<Vec<char>>();
+
+    let mut ans = 0;
+    let _ = loop {
+        for i in point..puzzle_len {
+            let current_char = puzzle_char[i as usize];
+
+            match count_map.get(&current_char) {
+                Some(value) => {
+                    count = 1;
+                    point = *value + 1;
+                    count_map.clear();
+                    break;
+                }
+                None => {
+                    count_map.insert(current_char, i);
+                    count += 1;
+                }
+            };
+            if count == 5 {
+                ans = i;
+                break;
+            }
+        }
+
+        if count == 5 {
+            break;
+        }
+    };
+    ans + 1
+}
+
 pub fn problem6_2() {}
 
 #[cfg(test)]
@@ -62,7 +102,13 @@ mod tests {
     #[test]
     fn tests_y2022_d6_1() {
         let puzzle_simple = "testdata/y2022_p6_simple.txt";
+        assert_eq!(problem6_1_another(puzzle_simple), 10);
+
+        let puzzle_simple = "testdata/y2022_p6_simple.txt";
         assert_eq!(problem6_1(puzzle_simple), 10);
+
+        let puzzle_simple = "testdata/y2022_p6.txt";
+        assert_eq!(problem6_1_another(puzzle_simple), 1262);
 
         let puzzle_simple = "testdata/y2022_p6.txt";
         assert_eq!(problem6_1(puzzle_simple), 1262);
